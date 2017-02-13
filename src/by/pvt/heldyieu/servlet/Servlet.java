cc419339772833fd304c9a10010f30cb6741a43a
HeroDishonest <xanderxander2015@yandex.ru>
2017-02-14 01:25:59 +0300

    commit

--- src/by/pvt/heldyieu/servlet/Servlet.java
+++ src/by/pvt/heldyieu/servlet/Servlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +16,7 @@ import by.pvt.heldyieu.command.ServletCommand;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/servlet")
+
 public class Servlet extends HttpServlet {
 	private static final Logger LOGGER = Logger.getLogger(Servlet.class);
 	private static final long serialVersionUID = 1L;
